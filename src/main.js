a1eb9ba98e0c3253c1d313907e04584f150bf87e
jingliu <jingliu8611@gmail.com>
2017-04-19 00:16:30 -0700

    implemented save data method to save current user's stocks / portfolio / funds into firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import {routes} from './routes'
 import store from './store/store'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://forec-bd810.firebaseio.com/public/';
 
 Vue.filter('currency', (value) => {
     return '$' + value.toLocaleString();

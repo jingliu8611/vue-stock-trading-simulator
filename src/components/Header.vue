c456271d82b9b4e9f6701668dda008e7eb3403d1
jingliu <jingliu8611@gmail.com>
2017-04-19 00:37:17 -0700

    implemented load data method, which loads saved data from firebase and updates the state

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,7 +18,7 @@
                            aria-expanded="false">Save & Load <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="#" @click="saveData">Save Data</a></li>
-                            <li><a href="#">Load Data</a></li>
+                            <li><a href="#" @click="loadData">Load Data</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -41,9 +41,10 @@
             }
         },
         methods: {
-            ...mapActions([
-                'randomizeStocks'
-            ]),
+            ...mapActions({
+                randomizeStocks: 'randomizeStocks',
+                fetchData: 'loadData'
+            }),
             endDay() {
                 this.randomizeStocks();
             },
@@ -54,6 +55,9 @@
                     stocks: this.$store.getters.stocks
                 };
                 this.$http.put('vue-stock-data.json', data);
+            },
+            loadData() {
+                this.fetchData();
             }
         }
     }
